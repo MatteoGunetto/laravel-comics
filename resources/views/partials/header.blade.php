9f075811b69dfd73e3d18c8063eb6a82cebe94ce
MatteoGunetto <Matteogunetto@tiscali.it>
2023-07-11 15:57:03 +0200

    print comics list no style

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,59 @@
+<header>
+    <div class="my-container">
+        <img src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="">
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">
+                        characters
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        comic
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        movies
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        tv
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        games
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        collectibles
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        video
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        fans
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        news
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        shops
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
---
+++ resources/views/partials/main.blade.php
@@ -0,0 +1,21 @@
+<main>
+    <section class="jumbotron">
+        <img src="{{ Vite::asset('resources/images/jumbotron.jpg') }}" alt="">
+        <button>current series</button>
+    </section>
+    <div class="my-container">
+        <section>
+            @foreach ($comics as $element)
+            <div class="my-card">
+                <img src="{{ $element['thumb'] }}" alt="">
+                <h6>
+                    {{ $element['title'] }}
+                </h6>
+            </div>
+            @endforeach
+        </section>
+        <div class="my-button">
+            <button>Load more</button>
+        </div>
+    </div>
+</main>
